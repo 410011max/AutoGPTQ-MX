c79e51f46f00e1f9453dc99016dba9b6603da898
410011max <410011max@gmail.com>
2024-02-08 02:45:43 +0800

    update scripts

--- scripts.sh
+++ scripts.sh
@@ -12,7 +12,7 @@ python examples/benchmark/perplexity.py \
 
 CUDA_VISIBLE_DEVICES=0,1 \
 python examples/benchmark/perplexity.py \
---model_name "meta-llama/Llama-2-7b-hf" \
+--model_name "meta-llama/Llama-2-13b-hf" \
 --n_ctx 512 --n_batch 512 --mx --mx_format int8 --mx_block_size 32
 
 CUDA_LAUNCH_BLOCKING=1
