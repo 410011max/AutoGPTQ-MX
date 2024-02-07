7717989368ec2910844c390bdd922dac18f5eecd
410011max <410011max@gmail.com>
2024-02-08 02:45:33 +0800

    add scripts

--- run3_model.sh
+++ run3_model.sh
@@ -1,10 +1,26 @@
 # get results from python, then push to array in bash
 model_name="meta-llama/Llama-2-13b-hf"
-mx_format_arr=(int8 fp6_e3m2 fp4 int4)
-mx_block_size_arr=(32)
+n_ctx_arr=(512)
+mx_format_arr=(int8 fp6_e3m2 fp6_e2m3 fp4 int4)
 
 # 2d ppl results array
 declare -A ppl_arr
+
+#function to get ppl from python get_ppl (model_name, n_ctx, mx_format)
+function get_ppl {
+    local model_name=$1
+    local n_ctx=$2
+    local mx_format=$3
+    local ppl=$(python examples/benchmark/perplexity.py --model_name $model_name --n_ctx $n_ctx --n_batch $n_ctx --mx --mx_format $mx_format --no_tqdm 2>&1)
+    echo $ppl | awk '{print $NF}'
+}
+
+function get_org_ppl {
+    local model_name=$1
+    local n_ctx=$2
+    local ppl=$(python examples/benchmark/perplexity.py --model_name $model_name --n_ctx $n_ctx --n_batch $n_ctx --no_tqdm 2>&1)
+    echo $ppl | awk '{print $NF}'
+}
 
 # function to get elapsed time, hour, minutes and seconds
 function elapsed_time {
@@ -14,38 +30,31 @@ function elapsed_time {
     echo "$minutes:$seconds"
 }
 
-#function to get ppl from python get_ppl (model_name, mx_format, mx_block_size)
-function get_ppl {
-    local model_name=$1
-    local mx_format=$2
-    local mx_block_size=$3
-    local ppl=$(python examples/benchmark/perplexity.py --model_name $model_name --mx --mx_format $mx_format --mx_block_size $mx_block_size --no_tqdm 2>&1)
-    echo $ppl | awk '{print $NF}'
-}
-
 # get ppl from python and push to array
-for mx_format in ${mx_format_arr[@]}
+for n_ctx in ${n_ctx_arr[@]}
 do
     SECONDS=0
-    for mx_bs in ${mx_block_size_arr[@]}
+    # echo "Running n_ctx: $n_ctx, original fp16..."
+    ppl_arr[$n_ctx,fp16]=$(get_org_ppl $model_name $n_ctx)
+    echo "ppl_arr[$n_ctx,fp16]: ${ppl_arr[$n_ctx,fp16]} --- Time elapsed: $(elapsed_time $SECONDS)"
+
+    for mx_format in ${mx_format_arr[@]}
     do
         SECONDS=0
-        ppl_arr[$mx_format,$mx_bs]=$(get_ppl $model_name $mx_format $mx_bs)
-        echo "ppl_arr[$mx_format,$mx_bs]: ${ppl_arr[$mx_format,$mx_bs]} --- Time elapsed: $(elapsed_time $SECONDS)"
+        # echo "Running n_ctx: $n_ctx, mx_format: $mx_format..."
+        ppl_arr[$n_ctx,$mx_format]=$(get_ppl $model_name $n_ctx $mx_format)
+        echo "ppl_arr[$n_ctx,$mx_format]: ${ppl_arr[$n_ctx,$mx_format]} --- Time elapsed: $(elapsed_time $SECONDS)"
     done
 done
 
 # print ppl array in table format
 echo -e "\n\n"
 echo -e "model_name: $model_name"
-echo -e "mx_format\t|int8\t|fp6_e3m2\t|fp4\t|int4"
+echo -e "n_ctx\t|int8\t|fp6_e3m2\t|fp6_e2m3\t|fp4\t|int4"
 echo -e "----------------------------------------"
-for mx_format in ${mx_format_arr[@]}
+for n_ctx in ${n_ctx_arr[@]}
 do
-    echo -e "$mx_format\t|${ppl_arr[$mx_format,8]}\t|${ppl_arr[$mx_format,16]}\t|${ppl_arr[$mx_format,32]}\t|${ppl_arr[$mx_format,64]}\t|${ppl_arr[$mx_format,128]}"
+    echo -e "$n_ctx\t|${ppl_arr[$n_ctx,fp16]}\t|${ppl_arr[$n_ctx,int8]}\t|${ppl_arr[$n_ctx,fp6_e3m2]}\t\t|${ppl_arr[$n_ctx,fp6_e2m3]}\t\t|${ppl_arr[$n_ctx,fp4]}\t|${ppl_arr[$n_ctx,int4]}"
 done
 echo -e "\n\n"
 exit 0
-
-
-
